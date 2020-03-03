1cd8846a8c21bab9931880453a357e4f535bfa68
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2020-03-03 10:05:42 +0100

    Use pyqtgraph; Refactor to  MainWindow/Tabs/Frame

--- GISANS_viewer8.py
+++ GISANS_viewer8.py
@@ -1,14 +1,13 @@
 #!/bin/env python
 
-from matplotlib.ticker import NullFormatter
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-
+#Qt stuff:
+from PyQt5.QtWidgets import QMainWindow, QFrame
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QDoubleSpinBox, QPushButton, QFormLayout, QMessageBox, QListWidget
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QLabel, QTableWidget
-from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QLabel, QTableWidget, QTabWidget
+from PyQt5.QtCore import Qt, pyqtSlot, QTimer
+
+#plot stuff:
+import pyqtgraph as pg
 
 import numpy as np
 import sys
@@ -56,146 +55,37 @@ class FrozenClass(object):
         self.__isfrozen = True
 
 
-class Canvas(FigureCanvas):
-    def __init__(self, parent=None, width=1000, height=28):  # , dpi = 500):
-        # definitions for the axes
-        left, width = 0.1, 0.65
-        bottom, height = 0.1, 0.65
-        spacing = 0.005
-        
-        rect_scatter = [left, bottom, width, height]
-        rect_histx = [left, bottom + height + spacing, width, 0.2]
-        rect_histy = [left + width + spacing, bottom, 0.2, height]
-        
-        # start with a rectangular Figure
-        fig = plt.figure(figsize=(10, 10))
-        
-        ax_scatter = plt.axes(rect_scatter)
-#        ax_scatter.tick_params(direction='in', top=True, right=True)
+class Canvas(pg.GraphicsLayoutWidget):
 
-        ax_histx = plt.axes(rect_histx)
-#        ax_histx.tick_params(direction='in', labelbottom=False)
+    def __init__(self):
+        pg.GraphicsLayoutWidget.__init__(self)
+        self.w2 = self.addViewBox()
+        self.w2.setAspectLocked(True)
 
-        ax_histy = plt.axes(rect_histy)
-#        ax_histy.tick_params(direction='in', labelleft=False)
-        
-       
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        self.ax_scatter = ax_scatter
-        self.ax_histx = ax_histx
-        self.ax_histy = ax_histy
-        self.colorbar = None
         #self.test()
+
         return
 
 
     def test(self):
-        x = np.linspace(1,10)
-        y = np.linspace(3,7)
-        X, Y = np.meshgrid(x, y)
-        #Z1 = np.exp(-X**2 - Y**2)
-        #Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
-        #I = (Z1 - Z2) * 2
-        #self.plot_colormap(I)
-        xcut = abs(np.cos(x))
-        ycut = abs(np.sin(y))
-        I = X + Y
-        self.scatter(x, y, I, x, y, xcut, ycut, vmin=1, vmax=10)
-        return True
-
-
-    def scatter(self, xarr, yarr, Iarr, xcutaxis, ycutaxis, xcut, ycut, vmin = None, vmax = None):
-        print("generating figure...")
-        if vmin is None:
-            vmin = min(Iarr)
-
-        if vmax is None:
-            vmax = min(Iarr)
-
-        fig = self.figure
-        ax_scatter = self.ax_scatter
-        ax_histx = self.ax_histx
-        ax_histy = self.ax_histy
-        #choice_size = 1024
-        #idx = np.random.choice(range(len(xarr)), size=choice_size, replace=False)
-        x = xarr#[idx]
-        y = yarr#[idx]
-        I = Iarr#[idx]
-
-        # the scatter plot:
-        im = ax_scatter.scatter(x, y, edgecolors='none', c=I, 
-                            norm = LogNorm(vmin=vmin, vmax=vmax),
-                            marker = '.')
-        #im = ax_scatter.contourf(x, y, I, 32,
-        #                    norm = LogNorm(vmin=vmin, vmax=vmax),
-        #                    linewidths=0,
-        #                    origin="image")
-
-        #contour_labels = ax_scatter.contour(x, y,
-        #                     I, 8, colors='black', linewidth=.5)
-        #ax_scatter.clabel(contour_labels, inline=1, fontsize=10)
-        #H, xedges, yedges = np.histogram2d(x, y, weights=I)
-        #im = ax_scatter.imshow(H, interpolation='bilinear', cmap='RdYlGn',
-        #       origin='lower', extent=[-3, 3, -3, 3],
-        #       vmax=abs(Iarr).max(), vmin=-abs(Iarr).max())
-        
-        # now determine nice limits by hand:
-        #ybinwidth = 
-        ax_scatter.set_xlim((x.min(), x.max()))
-        ax_scatter.set_ylim((y.min(), y.max()))
-
-        #bins = np.arange(x.min(), x.max() + binwidth, binwidth)
-        #ax_histx.hist(x, bins=bins)
-        ax_histx.scatter(xcutaxis, xcut)
-        ax_histx.set_yscale('log')
-        ax_histx.set_xlim(ax_scatter.get_xlim())
-        #ax_histx.set_ylim((0.5*xcut.min(), 2*xcut.max()))
-        #ax_histy.hist(y, bins=bins, orientation='horizontal')
-        
-        #ax_histx.set_xlim(ax_scatter.get_xlim())
-        ax_histy.scatter(ycut, ycutaxis)
-        ax_histy.set_xscale('log')
-        ax_histy.set_ylim(ax_scatter.get_ylim())
-        #ax_histy.set_xlim((0.5*ycut.min(), 2*ycut.max()))
- 
-###
-        ax_scatter.set_title("GISANS map")
-        self.colorbar = self.figure.colorbar(im)
-        self.colorbar.set_label("Intensity")
-        ax_scatter.set_xlabel(r'$Q_{y}(\AA^{-1})$')
-        ax_scatter.set_ylabel(r'$Q_{z}(\AA^{-1})$') 
-        #ax_histx.set_xlabel("ax_histx")
-        #ax_histy.set_xlabel("ax_histy")
-        print("Figure Generated")
+        n = 1000
+        x = np.random.normal(size=n)
+        y = np.random.normal(size=n)
+        X,Y = np.meshgrid(x,y)
+        c = x**2 + y**2
+        self.scatter(x,y,c)
 
         return True
 
 
-    def plot_colormap(self, xarr, yarr, Iarr, vmin=None, vmax=None, extent = [-1,1,-1,1]):
-
-
-        H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
-        H = H.T  # Let each row list bins with common y range.
-        print("generating figure...")
-        if vmin is None:
-            vmin = Iarr.min()
-
-        if vmax is None:
-            vmax = Iarr.max()
-
-        fig = self.figure
-        axes = self.axes
-        im = axes.imshow(Iarr, interpolation='bilinear', cmap='RdYlGn',
-               origin='lower', extent=[-3, 3, -3, 3],
-               vmax=abs(Iarr).max(), vmin=-abs(Iarr).max())
-
-        axes.set_title("GISANS map")
-        # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-        cbar = fig.colorbar(im)#, cax=cbar_ax)
-        cbar.set_label("Intensity", fontsize=20)
-        plt.ylabel(r'$Q_{y}(\AA^{-1})$')
-        plt.xlabel(r'$Q_{z}(\AA^{-1})$') 
+    def scatter(self, xvals, yvals, cvals):
+        print("Scatter plot...")
+        n = len(xvals)
+        curve = pg.ScatterPlotItem(x=xvals, y=yvals, size=5, pen=pg.mkPen(None), pxMode=True)
+        spots = [{'pos': (xvals[i],yvals[i]), 'brush':pg.mkColor(cvals[i])} for i in range(n)]
+        #curve.addPoints(spots)
+        self.w2.addItem(curve)
+        return True
 
 
 class Experiment(FrozenClass):
@@ -298,54 +188,72 @@ class Settings(FrozenClass):
         return os.path.join(self.basename(),f"_line_cut_q{y_or_z}.out")
 
 
-class App(QWidget,FrozenClass):
-
+class App(QMainWindow,FrozenClass):
     def __init__(self):
         super().__init__()
         self.title = 'Alexandros GISANS Viewer'
-        #self.left = 3000
-        #self.top = 500
-        self.width = 1500
-        self.height = 1500
+        self.myTabs = MyTabs()
+        self.setCentralWidget(self.myTabs)
+        self._freeze()
+        self.setWindowTitle(self.title)
+        self.show()
+
+
+    def addTab(self):
+        self.myTabs.addTab()
+
+
+class MyTabs(QTabWidget,FrozenClass):
+    def __init__(self):
+        super().__init__()
+        self.addTab()
+        q = QTabWidget()
+
+
+    def addTab(self):
+        frame = MyFrame()
+        super().addTab(frame, "tab" + str(self.count()))
+        return
+
+
+
+class MyFrame(QFrame,FrozenClass):
+
+    def __init__(self):
+        super().__init__()
         self.layout = QHBoxLayout()
         self.centralpanel = QVBoxLayout()
         self.leftpanel = QVBoxLayout()
         self.rightpanel = QVBoxLayout()
-        self.addExperimentInfo()
         self.minSpinBox = QDoubleSpinBox()
         self.maxSpinBox = QDoubleSpinBox()
         self.settings = Settings()
         self.experiment = Experiment()
         self.canvas = Canvas()
-
-
-
+        self.infoTable = QTableWidget()
+        self.fileList = QListWidget()
+        self.tabs = QTabWidget()
+        self.initFrame()
         self._freeze()
-        self.initUI()
 
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        #self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setLayout(self.layout)
+    def initFrame(self):
         self.layout.setAlignment(Qt.AlignCenter)
+        self.addExperimentInfo()
         self.addFileList()
         self.addWelcomeMessage()
         self.addCanvas()
         self.addMinMaxSpinBoxes()
         self.addFunctionalityButtons()
         self.addPanels()
-        self.show()
-        # self.openFileNamesDialog()
-        # self.saveFileDialog()
+        self.setLayout(self.layout)
 
 
     def addFileList(self):
-        fileList = QListWidget()
+        self.fileList.setMaximumWidth(self.fileList.width()/2.)
         self.leftpanel.addWidget(QLabel("File:"))
-        self.leftpanel.addWidget(fileList)
+        self.leftpanel.addWidget(self.fileList)
         return
-
 
 
     def addPanels(self):
@@ -356,9 +264,9 @@ class App(QWidget,FrozenClass):
 
 
     def addExperimentInfo(self):
-        infoTable = QTableWidget()
+        self.infoTable.setMaximumWidth(self.infoTable.width()/2.)
         self.rightpanel.addWidget(QLabel("Info:"))
-        self.rightpanel.addWidget(infoTable)
+        self.rightpanel.addWidget(self.infoTable)
         return
 
 
@@ -415,6 +323,7 @@ class App(QWidget,FrozenClass):
         self.centralpanel.addWidget(message)
         return
 
+
     def addCanvas(self):
         self.centralpanel.addWidget(self.canvas)
         return
@@ -441,12 +350,16 @@ class App(QWidget,FrozenClass):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Measurement dat file (*.dat)", options=options)
+        # self.openFileNamesDialog()
+        # self.saveFileDialog()
         if fileName:
             return fileName 
         return None
 
 
     def doStuff(self):
+        #self.canvas.test()
+        #return
         if not self.read_cbar_min_max():
             return
         if not self.read_dat_file():
@@ -464,6 +377,7 @@ class App(QWidget,FrozenClass):
         if not self.show_gisans_map():
             return
         return
+
 
     def read_cbar_min_max(self):
         self.settings.cbar_min = self.minSpinBox.value()
@@ -661,7 +575,7 @@ class App(QWidget,FrozenClass):
         print("Performing line cut y")
 
         self.experiment.cut_qy_xaxis = np.linspace(-1,1)
-        self.experiment.cut_qy = np.sin(self.experiment.cut_qy_xaxis)
+        self.experiment.cut_qy = 10 + np.sin(self.experiment.cut_qy_xaxis)
         return True
 #####TODO
         sens = self.experiment.sens
@@ -698,7 +612,7 @@ class App(QWidget,FrozenClass):
         print("Performing line cut z")
 
         self.experiment.cut_qz_xaxis = np.linspace(-1,1)
-        self.experiment.cut_qz = np.cos(self.experiment.cut_qz_xaxis)
+        self.experiment.cut_qz = 10 + np.cos(self.experiment.cut_qz_xaxis)
         return True
 ###TODO
         sens = self.experiment.sens
@@ -745,12 +659,12 @@ class App(QWidget,FrozenClass):
             self.canvas.scatter(self.experiment.qy,
                             self.experiment.qz,
                             self.experiment.I,
-                            self.experiment.cut_qy_xaxis,
-                            self.experiment.cut_qz_xaxis,
-                            self.experiment.cut_qy,
-                            self.experiment.cut_qz,
-                            vmin=min_value,
-                            vmax=max_value
+                            #self.experiment.cut_qy_xaxis,
+                            #self.experiment.cut_qz_xaxis,
+                            #self.experiment.cut_qy,
+                            #self.experiment.cut_qz,
+                            #vmin=min_value,
+                            #vmax=max_value
                             )
         except Exception as e:
             App.handle_exception(e)
@@ -764,204 +678,3 @@ if __name__ == '__main__':
     #sys.exit(app.exec_())
     x = profile_function_with_arguments(app.exec_)
     sys.exit(x)
-
-
-
-exit(0)
-
-
-minmax = raw_input("enter min/max (yes/no): ")
-# minmax='no'
-if minmax=='yes':
-    gmin=input('Give min value:');
-    gmax=input('Give max value:');
-
-# qzvalue=0.0
-# dqzvalue=0.05
-# qzvalue=input("Give qz: ");
-# dqzvalue=input("Give delta qz: ");
-
-# Open file dialog
-file_path_string = openFileNameDialog()
-
-   
-basename = file_path_string[:-4]
-
-# Open and read the dat file
-file = open(file_path_string, 'r') 
-
-for line in file:
-    if line.find('omega_value')>0:
-        omega_line_list = line.split()
-        omega=omega_line_list[3]
-        print('Angle of incidence (degrees): '+str(omega))
-    if line.find('selector_lambda_value')>0:
-        lambda_line_list = line.split()
-        selector_lambda=lambda_line_list[3]
-        print('Neutron wavelength (Angtrom): '+str(selector_lambda))
-    if line.find('.gz')>0:
-        line_list = line.split()
-        for word in line_list:
-            if word.find('.gz')>0:
-                file_path_string = word
-    if line.find('.yaml')>0:
-        line_list = line.split()
-        for word in line_list:
-            if word.find('.yaml')>0:
-                file_path_stringB = word
-
-file.close()
-
-# Open and read the yaml file
-line1=''
-line2=''
-line3=''
-line4=''
-file = open(file_path_stringB, 'r') 
-for line in file:
-    if line4.find('name: mon1')>0:
-        line_list=line1.split()
-        monitor=line_list[1]
-        print('Monitor conuts: '+str(monitor)+'\n')
-    line4=line3
-    line3=line2
-    line2=line1
-    line1=line
-
-file.close()
-
-# Define the position of the direct beam on the detector
-yc=528 #530
-zc=211 #220
-# zc=512
-
-y=[]
-z=[]
-I=[]
-
-print('Loading data and applying corrections... please wait..'+'\n')
-
-# Unzip the file
-os.system('cp '+file_path_string+' temp.gz')
-os.system('gzip -d -q temp.gz')
-
-
-sens = np.loadtxt("sensitivity_map", dtype='i', delimiter=' ')
-# sens = np.loadtxt("sens.map", dtype='i', delimiter=' ')
-
-num=0.0
-meansens=0.0
-for i in xrange(1,1024):
-     for j in xrange(1,1024):
-         if i>=162 and i<=862:
-             if j>=162 and j<=862:
-                 meansens=meansens+float(sens[i,j])
-                 num=num+1.0
-
-meansens=meansens/num
-
-inputd = np.loadtxt("temp", dtype='i', delimiter=' ')
-os.system('rm temp')
-# os.system('rm temp.gz')
-
-# file = open('output.dat','w') 
-
-# Addition important... qz=0 at the horizon of the sample
-
-print(zc)
-zc=zc+int((1990.0*np.tan(np.pi*float(omega)/180.0))/0.5755)
-print(zc)
-
-file = open(basename+"_GISANS.map", "w")
-
-for i in xrange(1,1024):
-     for j in xrange(1,1024):
-         if i>=162 and i<=862:
-             if j>=162 and j<=862:
-                 y.append((2*np.pi/float(selector_lambda))*np.cos(np.pi*float(omega)/180.0)*(0.5755*(yc-i)/np.sqrt(1990*1990+(0.5755*(yc-i))*(0.5755*(yc-i)))))
-                 z.append((2*np.pi/float(selector_lambda))*(np.sin(np.pi*float(omega)/180.0)+(0.5755*(j-zc)/np.sqrt(1990*1990+(0.5755*(zc-j))*(0.5755*(zc-j))))))
-                 if float(sens[i,j]) > 0.0:
-                     I.append((meansens*float(inputd[i,j])/float(sens[i,j]))/float(monitor))
-                     file.write(str((2*np.pi/float(selector_lambda))*np.cos(np.pi*float(omega)/180.0)*(0.5755*(yc-i)/np.sqrt(1990*1990+(0.5755*(yc-i))*(0.5755*(yc-i)))))+'   '+str((2*np.pi/float(selector_lambda))*(np.sin(np.pi*float(omega)/180.0)+(0.5755*(j-zc)/np.sqrt(1990*1990+(0.5755*(zc-j))*(0.5755*(zc-j))))))+'   '+str((meansens*float(inputd[i,j])/float(sens[i,j]))/float(monitor))+'\n')
-                    # file.write(str((2*np.pi/float(selector_lambda))*np.cos(np.pi*float(omega)/162.0)*((yc-i)/np.sqrt(1990*1990+(0.6*(yc-i))*(0.6*(zc-i)))))+' '+str((-2*np.pi/float(selector_lambda))*(np.sin(np.pi*float(omega)/162.0)+((zc-j)/np.sqrt(1990*1990+(0.6*(zc-j))*(0.6*(zc-j))))))+' '+str(float(input[i,j])/float(monitor))+'\n')
-
-file.close()
-
-min_value=min(I)+float(1)/float(monitor)
-
-
-max_value=max(I)
-
-if minmax=='yes':
-    min_value=gmin
-    max_value=gmax
-
-
-plt.scatter(z,y,edgecolors='none',c=I, norm = LogNorm(vmin=min_value, vmax=max_value), marker = 's')
-cbar = plt.colorbar()
-plt.ylabel(r'$Q_{y}(\AA^{-1})$')
-plt.xlabel(r'$Q_{z}(\AA^{-1})$') 
-plt.show()
-
-
-linecut = raw_input("Line cut (yes/no): ");
-
-qdirection=' '
-if linecut=='yes':
-    qdirection=raw_input('qy or qz? ');
-
-
-if qdirection=='qy' and linecut=='yes':
-    qzvalue=input("Give qz: ");
-    dqzvalue=input("Give delta qz: ");
-
-    cut_qy=[0] * len(z)
-    cut_qy_xaxis=[0] * len(z)
-    for i in xrange(1,1024):
-        for j in xrange(1,1024):
-            if i>=162 and i<=862:
-                if j>=162 and j<=862:
-                    cut_qy_xaxis[i]=((2*np.pi/float(selector_lambda))*np.cos(np.pi*float(omega)/180.0)*(0.5755*(yc-i)/np.sqrt(1990*1990+(0.5755*(yc-i))*(0.5755*(yc-i)))))
-                    cqz=((2*np.pi/float(selector_lambda))*(np.sin(np.pi*float(omega)/180.0)+(0.5755*(j-zc)/np.sqrt(1990*1990+(0.5755*(zc-j))*(0.5755*(zc-j))))))
-                    if cqz>=qzvalue-dqzvalue and cqz<=qzvalue+dqzvalue:
-                        cut_qy[i]=cut_qy[i]+(meansens*float(inputd[i,j])/float(sens[i,j]))/float(monitor)
-    plt.loglog(cut_qy_xaxis,cut_qy,label="cut at "+r'$Q_{z}$= '+'{:06.4f}'.format(qzvalue)+'$ \AA^{-1})$'+'\n'+r'$\Delta Q_{z}$= '+'{:06.4f}'.format(dqzvalue)+'$ \AA^{-1})$')
-    plt.legend()
-    plt.ylabel(r'$I(A.U.)$')
-    plt.xlabel(r'$Q_{y}(\AA^{-1})$')
-    plt.show()
-
-
-    file = open(basename+"_line_cut_qy.out", "w")
-    for i in xrange(1,1024):
-        if i>=162 and i<=862:
-            file.write(str(cut_qy_xaxis[i])+'   '+str(cut_qy[i])+"\n")
-    file.close()
-
-if qdirection=='qz' and linecut=='yes':
-    qyvalue=input("Give qy: ");
-    dqyvalue=input("Give delta qy: ");
-
-    cut_qz=[0] * len(y)
-    cut_qz_xaxis=[0] * len(y)
-    for i in xrange(1,1024):
-        for j in xrange(1,1024):
-            if i>=162 and i<=862:
-                if j>=162 and j<=862:
-                    cut_qz_xaxis[j]=((2*np.pi/float(selector_lambda))*(np.sin(np.pi*float(omega)/180.0)+(0.5755*(j-zc)/np.sqrt(1990*1990+(0.5755*(zc-j))*(0.5755*(zc-j))))))
-                    cqy=((2*np.pi/float(selector_lambda))*np.cos(np.pi*float(omega)/180.0)*(0.5755*(yc-i)/np.sqrt(1990*1990+(0.5755*(yc-i))*(0.5755*(yc-i)))))
-                    if cqy>=qyvalue-dqyvalue and cqy<=qyvalue+dqyvalue:
-                        cut_qz[j]=cut_qz[j]+(meansens*float(inputd[i,j])/float(sens[i,j]))/float(monitor)
-    plt.loglog(cut_qz_xaxis,cut_qz,label="cut at "+r'$Q_{y}$= '+'{:06.4f}'.format(qyvalue)+'$ \AA^{-1})$'+'\n'+r'$\Delta Q_{y}$= '+'{:06.4f}'.format(dqyvalue)+'$ \AA^{-1})$')
-    plt.legend()
-    plt.ylabel(r'$I(A.U.)$')
-    plt.xlabel(r'$Q_{z}(\AA^{-1})$')
-    plt.show()
-
-
-    file = open(basename+"_line_cut_qz.out", "w")
-    for i in xrange(1,1024):
-        if i>=162 and i<=862:
-            omega_value=cut_qz_xaxis[i]*float(selector_lambda)*180.0/(4.0*np.pi*np.pi)
-            file.write(str(omega_value)+'   '+str(cut_qz_xaxis[i])+'   '+str(cut_qz[i])+"\n")
-    file.close()
